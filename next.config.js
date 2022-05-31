ecbbf8762a040dec703a613015ecf34d7c8b0804
neillycubed <neillybusiness3@gmail.com>
2022-05-31 00:47:32 -0700

    init

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    reactStrictMode: true,
+    images: {
+      domains: ["rickandmortyapi.com"],
+      loader: "custom",
+      path: "/",
+    },
+  };
---
+++ webpack/webpack.config.ts
@@ -0,0 +1,71 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    "mode": process.env.NODE_ENV === "production" ? "production" : "development",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: "file-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader"
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "styles.css",
+            chunkFilename: "styles.css"
+        }),
+        new webpack.DefinePlugin({
+            "process.env": {
+                "NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+        new Dotenv()
+    ],
+    devServer: {
+        historyApiFallback: true,
+        port: 3000
+    }
+}
