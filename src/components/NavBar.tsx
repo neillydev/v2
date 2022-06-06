57f3f649f6c6447d9907a9da1efcdca7d085f741
neillycubed <neillybusiness3@gmail.com>
2022-06-05 19:21:16 -0700

    add NavItems to NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,28 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
+import styles from '../../styles/NavBar.module.css';
+import NavItem from "./NavItem";
+
 export default function NavBar(){
     const router = useRouter();
 
     return (
-        <header className='navHeader'>
-            <nav className='nav'>
+        <header className={styles.navHeader}>
+            <nav className={styles.nav}>
+                <div className={styles.logo}>
+                    <a href="/">
 
+                    </a>
+                </div>
+                <div className={styles.navList}>
+                    <ol>
+                        <NavItem destination="/#about" itemText="About" />
+                        <NavItem destination="/#experience" itemText="Experience" />
+                        <NavItem destination="/#projects" itemText="Projects" />
+                        <NavItem destination="/#contact" itemText="Contact" />
+                    </ol>
+                </div>
             </nav>
         </header>
     )
