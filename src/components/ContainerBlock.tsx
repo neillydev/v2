61b57e15642bf8cd7d56c17ac2cf293fcfdcc957
neillycubed <neillybusiness3@gmail.com>
2022-06-06 23:45:56 -0700

    refactor component structure

--- src/components/ContainerBlock.tsx
+++ src/components/ContainerBlock.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import NavBar from './NavBar';
+
+import styles from '../../styles/main.module.css';
 
 export default function ContainerBlock({ children, ...customMeta }: any) {
   const router = useRouter();
@@ -40,11 +43,16 @@ export default function ContainerBlock({ children, ...customMeta }: any) {
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
-      <main className="dark:bg-gray-800 w-full">
-       
-        <div>{children}</div>
+      <div className="containerBlock">
+        <NavBar />
         
-      </main>
+        <div className="mainContent">
+            <main className={styles.mainContainer}>
+              {children}
+            </main>
+        </div>
+        
+      </div>
     </div>
   );
 }
