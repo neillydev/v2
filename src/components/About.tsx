a49275f2ff9f1913a29d5bb9bd7b794dd08b010d
neillycubed <neillybusiness3@gmail.com>
2022-06-12 15:05:39 -0700

    create About component

---
+++ src/components/About.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import styles from '../../styles/About.module.css';
+
+const About = () => {
+    return (
+        <section className={styles.aboutContainer}>
+            <div className={styles.aboutContent}>
+                <div className={styles.aboutHeader}>
+                    <h2>
+                        About Me
+                    </h2>
+                </div>
+                <div className={styles.aboutBody}>
+                    <p>Hi! My name is Vernon Neilly, and programming is my passion! From a young age, I've taken an interest in the internet; developing a burning desire to understand how everything worked under the hood.</p>
+                    <p>From humble beginnings, creating websites with HTML, Javascript, and CSS, to building plugins and web applications for various clients, to working with a company, I've had the privilege to procure valuable experience from various different avenues.</p>
+                    <p>Below are technologies I work with consistently:</p>
+                    <ul className={styles.aboutTechnologies}>
+                        <li>JavaScript</li>
+                        <li>TypeScript</li>
+                        <li>Node.js</li>
+                        <li>Python</li>
+                        <li>React</li>
+                        <li>Angular</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={styles.aboutImg}>
+                <div className={styles.aboutImgContainer}>
+                    <picture>
+                        <img className={styles.aboutImgHeadshot} width="500" height="500" src="https://i.imgur.com/YHOjM4H.jpg" />
+                    </picture>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default About;
