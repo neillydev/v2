7a3359de945a5fba1907222cecb19d63979c0bf0
neillycubed <neillybusiness3@gmail.com>
2022-06-20 21:23:25 -0700

    refactor Projects structure and styling

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,17 +21,34 @@ const Projects = () => {
                 </div>
                 <div className={styles.projItem}>
                     <div className={styles.projItemWrapper}>
-                        <a href="https://waves.neilly.dev" className={styles.projItemImg} target="_blank" rel="noreferrer noopener"></a>
-                        <img className={styles.projItemImg} src="https://camo.githubusercontent.com/93f69877443045fe783bbc15a7746137eb6d30500a8fd2d5ce783db04b2f8dcd/68747470733a2f2f692e6779617a6f2e636f6d2f39376264323633623931666362633232393638373931356261303936396330622e706e67" alt="" style={{ width: "100%", height: "100%" }} />
+                        <span>
+                            <img className={styles.projItemImg} src="https://camo.githubusercontent.com/93f69877443045fe783bbc15a7746137eb6d30500a8fd2d5ce783db04b2f8dcd/68747470733a2f2f692e6779617a6f2e636f6d2f39376264323633623931666362633232393638373931356261303936396330622e706e67"
+                                alt=""
+                                style={{ width: "100%", height: "100%" }} />
+                            <a href="https://waves.neilly.dev"
+                                target="_blank" rel="noreferrer noopener" className={styles.projItemOverlay}>
+                            </a>
+                        </span>
                     </div>
                 </div>
                 <div className={styles.projCardContainer}>
                     <h3 className={styles.projCardHeader}>
-                        <a href="https://waves.neilly.dev">Waves</a>
+                        <a href="https://waves.neilly.dev" target="_blank" rel="noreferrer noopener">Waves</a>
                     </h3>
                     <div className={styles.projCard}>
                         <p>A video sharing social media app that allows anybody to share their content with the world</p>
                     </div>
+                    <ul className={styles.projTechnologies}>
+                        <li>
+                            React
+                        </li>
+                        <li>
+                            Node JS
+                        </li>
+                        <li>
+                            PostgreSQL
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
