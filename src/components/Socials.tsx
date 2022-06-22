0189d3c1b65f978e3a73f154696929604a9f4772
neillycubed <neillybusiness3@gmail.com>
2022-06-21 20:19:38 -0700

    add social media buttons to widget

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 import styles from '../../styles/Socials.module.css';
+import Github from '../../public/svg/github.svg';
+import LinkedIn from '../../public/svg/linkedin.svg';
+import Twitter from '../../public/svg/twitter.svg';
 
 const Socials = () => {
   return (
     <div className={styles.socialsContainer}>
       <ul className={styles.socialsList}>
         <li className={styles.socialsListItem}>
-          <a href='https://github.com/neillydev' target='_blank' rel='noreferrer noopener'> 
-
+          <a href='https://github.com/neillydev' target='_blank' rel='noreferrer noopener' aria-label='Github'>
+            <Github />
+          </a>
+        </li>
+        <li className={styles.socialsListItem}>
+          <a href='https://www.linkedin.com/in/vernon-neilly-iii-2508a6190/' target='_blank' rel='noreferrer noopener' aria-label='LinkedIn'>
+            <LinkedIn />
+          </a>
+        </li>
+        <li className={styles.socialsListItem}>
+          <a href='https://twitter.com/neillydev' target='_blank' rel='noreferrer noopener' aria-label='Twitter'>
+            <Twitter />
           </a>
         </li>
       </ul>
