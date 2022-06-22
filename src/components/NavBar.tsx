6c2fadc005bbc7cf366b80fdb0927a4fe61bf806
neillycubed <neillybusiness3@gmail.com>
2022-06-21 20:55:06 -0700

    add scroll position detection to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useScrollPosition } from "../hooks/useScrollPosition";
 
 import styles from '../../styles/NavBar.module.css';
 import NavItem from "./NavItem";
 
+
+const classNames = (...classes: any) => {
+    return classes.filter(Boolean).join('');
+}
+
 export default function NavBar(){
     const router = useRouter();
+    const scrollPosition = useScrollPosition();
 
     return (
-        <header className={styles.navHeader}>
+        <header className={`${styles.navHeader} ${scrollPosition > 0 ? (scrollPosition > 125 ? styles.navHeaderScrolled : styles.navHeaderScrolling) : ''}`}>
             <nav className={styles.nav}>
                 <div className={styles.logo}>
                     <a href="/">
