4f3bff92160da5ace2c7231a08d0a584869c08ab
neillycubed <neillybusiness3@gmail.com>
2022-06-27 21:44:56 -0700

    create contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from "framer-motion";
 
 import styles from '../../styles/Contact.module.css';
 
 const Contact = () => {
+    const [showContact, setShowContact] = useState(false);
+
+    const handleContactBtn = () => {
+        setShowContact(!showContact);
+    };
+
     return (
         <section className={styles.contactSection}>
             <motion.aside
@@ -26,9 +32,23 @@ const Contact = () => {
                 <p>
                     Send me a message if you have a question, or want to get in touch!
                 </p>
-                <button className={styles.contactBtn}>
-                    Say Hi
+                <button className={styles.contactBtn} onClick={handleContactBtn}>
+                    {!showContact ? `Say Hi` : `Show less`}
                 </button>
+                <form className={styles.contactForm}>
+                    <div className={styles.contactWrapper}>
+                        <input className={styles.contactInput} type="text" id="name" placeholder=' ' required />
+                        <label className={styles.contactLabel}  htmlFor="name">Name</label>
+                    </div>
+                    <div className={styles.contactWrapper}>
+                        <input className={styles.contactInput}type="text" id="email" placeholder=' ' required />
+                        <label className={styles.contactLabel} htmlFor="email">Email</label>
+                    </div>
+                    <div className={styles.contactWrapper}>
+                        <input className={styles.contactInput} type="text" id="message" placeholder=' ' required />
+                        <label className={styles.contactLabel} htmlFor="message">Message</label>
+                    </div>
+                </form>
             </motion.aside>
         </section>
     )
