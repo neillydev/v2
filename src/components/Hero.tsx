ec039620269f7efc6785472848d79aa3db6898cf
neillycubed <neillybusiness3@gmail.com>
2022-06-27 22:48:32 -0700

    change Hero body text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,6 +4,7 @@ import styles from '../../styles/Hero.module.css';
 import CodeEditor from './CodeEditor';
 
 const Hero = () => {
+
   return (
     /* 
     Main part introducing myself on the left side, with the right side having a dark-mode code editor explaining either my languages, experience, or the intro itself in the
@@ -15,24 +16,24 @@ const Hero = () => {
     <section className={styles.heroSection}>
       <div className={styles.heroSectionContainer}>
         <div className={styles.heroItemContainer}>
-          <div className={styles.heroItem} style={{ transitionDelay: '100ms' }}>
+          <div className={`${styles.heroItem} ${styles.heroItem1}`} style={{  }}> 
             <h1 className={styles.heroIntro}>
               Hello, my name is
             </h1>
           </div>
-          <div className={styles.heroItem} style={{ transitionDelay: '200ms' }}>
+          <div className={`${styles.heroItem} ${styles.heroItem2}`} style={{  }}>
             <h2 className={styles.heroHeader}>
               Vernon Neilly
             </h2>
           </div>
-          <div className={styles.heroItem} style={{ transitionDelay: '300ms' }}>
+          <div className={`${styles.heroItem} ${styles.heroItem3}`} style={{  }}>
             <h3 className={`${styles.heroHeader} ${styles.heroHeaderFade}`}>
               Full Stack Developer
             </h3>
           </div>
-          <div className={styles.heroItem} style={{ transitionDelay: '400ms' }}>
+          <div className={`${styles.heroItem} ${styles.heroItem4}`} style={{  }}>
             <p>
-              I'm a Full Stack Developer with over 10 years of experience engineering remarkable solutions. Currently, I'm developing practical solutions for staffing at&nbsp;
+              I'm a Full Stack Developer with over 10 years of experience engineering remarkable solutions. Currently, I'm developing applications for staffing at&nbsp;
               <a href="https://exactstaff.com" target="_blank" rel="noreferrer">
                 Exact Staff
               </a>
