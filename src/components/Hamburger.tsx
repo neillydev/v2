fe8f3624f3e0fdd24c167b896759f93d905bf194
neillycubed <neillybusiness3@gmail.com>
2022-07-15 02:01:57 -0700

    create hook for preventing scrolling

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import { useNav, useNavUpdate } from "../context/NavContext";
+import { useScrollPrevent } from "../hooks/useScrollPrevent";
 
 import styles from '../../styles/Hamburger.module.css';
 
 const Hamburger = () => {
+  const navOpen = useNav();
   const toggleNav = useNavUpdate();
+  const [allowScroll, blockScroll] = useScrollPrevent();
+
+  const handleHamburger = () => {
+    toggleNav();
+    !navOpen ? allowScroll() : blockScroll();
+  };
 
   return (
-    <div className={styles.hamburgerContainer} onClick={toggleNav}>
+    <div className={styles.hamburgerContainer} onClick={handleHamburger}>
         <span></span>
         <span></span>
         <span></span>
