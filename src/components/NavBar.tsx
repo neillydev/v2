d01eb34d09c3694beda09ef4641e16ade4c82382
neillycubed <neillybusiness3@gmail.com>
2022-07-18 00:07:13 -0700

    add resume loading to resume button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
-import { useRouter } from "next/router";
-import { useScrollPosition } from "../hooks/useScrollPosition";
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useScrollPosition } from '../hooks/useScrollPosition';
+import resumePDF from '../../public/pdf/resume.pdf';
 
 import styles from '../../styles/NavBar.module.css';
-import NavItem from "./NavItem";
-import Hamburger from "./Hamburger";
+import NavItem from './NavItem';
+import Hamburger from './Hamburger';
 
 const classNames = (...classes: any) => {
     return classes.filter(Boolean).join('');
@@ -19,16 +20,16 @@ export default function NavBar({ refs }: NavProps) {
     const scrollPosition = useScrollPosition();
     useEffect(() => {
         switch (router.asPath) {
-            case "/#about":
+            case '/#about':
                 scrollSmoothHandler(refs.aboutRef);
                 break;
-            case "/#experience":
+            case '/#experience':
                 scrollSmoothHandler(refs.experienceRef);
                 break;
-            case "/#projects":
+            case '/#projects':
                 scrollSmoothHandler(refs.projectsRef);
                 break;
-            case "/#contact":
+            case '/#contact':
                 scrollSmoothHandler(refs.contactRef);
                 break;
             default:
@@ -37,26 +38,26 @@ export default function NavBar({ refs }: NavProps) {
     }, [router.asPath, refs]);
 
     const scrollSmoothHandler = (ref: any) => {
-        ref.current.scrollIntoView({ behavior: "smooth" });
+        ref.current.scrollIntoView({ behavior: 'smooth' });
     };
 
     return (
         <header className={`${styles.navHeader} ${scrollPosition > 0 ? (scrollPosition > 125 ? styles.navHeaderScrolled : styles.navHeaderScrolling) : ''}`}>
             <nav className={styles.nav}>
                 <div className={styles.logo}>
-                    <a href="/">
+                    <a href='/'>
 
                     </a>
                 </div>
                 <Hamburger />
                 <div className={styles.navList}>
                     <ol>
-                        <NavItem destination="/#about" itemText="About" />
-                        <NavItem destination="/#experience" itemText="Experience" />
-                        <NavItem destination="/#projects" itemText="Projects" />
-                        <NavItem destination="/#contact" itemText="Contact" />
+                        <NavItem destination='/#about' itemText='About' />
+                        <NavItem destination='/#experience' itemText='Experience' />
+                        <NavItem destination='/#projects' itemText='Projects' />
+                        <NavItem destination='/#contact' itemText='Contact' />
                         <div className={styles.resumeContainer}>
-                            <a className={styles.resumeBtn} href="/vernon_neilly.pdf">Resume</a>
+                            <a className={styles.resumeBtn} href={resumePDF} target='_blank'>Resume</a>
                         </div>
                     </ol>
                 </div>
